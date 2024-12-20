0e6d48a21fe19e9bf33aa541f82c7b5980fa4dff
kmilos-foi <kmilos21@foi.hr>
2024-12-20 21:39:02 +0100

    Inicijalni projekt

---
+++ src/main/java/foi/air/szokpt/transfetch/TransfetchApplication.java
@@ -0,0 +1,13 @@
+package foi.air.szokpt.transfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfetchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/foi/air/szokpt/transfetch/TransfetchApplicationTests.java
@@ -0,0 +1,13 @@
+package foi.air.szokpt.transfetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
