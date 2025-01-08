5b7638c5cc0a8d323b9c459d6176eeceb4b5fedd
kmilos-foi <kmilos21@foi.hr>
2025-01-08 16:59:48 +0100

    MI5-88 Dohvaćanje podataka kod pokretanja servera
    @PostConstruct omogućava da se ova metoda pozove odmah kada se server pokrene.

--- src/main/java/foi/air/szokpt/transfetch/schedulers/DataFetchScheduler.java
+++ src/main/java/foi/air/szokpt/transfetch/schedulers/DataFetchScheduler.java
@@ -1,6 +1,7 @@
 package foi.air.szokpt.transfetch.schedulers;
 
 import foi.air.szokpt.transfetch.handlers.DataFetchHandler;
+import jakarta.annotation.PostConstruct;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ public class DataFetchScheduler {
         this.dataFetchHandler = fetchService;
     }
 
+    @PostConstruct
     @Scheduled(cron = "0 0 0,12 * * ?", zone = "Europe/Zagreb")
     public void periodicDataFetch() {
         dataFetchHandler.fetchData();
