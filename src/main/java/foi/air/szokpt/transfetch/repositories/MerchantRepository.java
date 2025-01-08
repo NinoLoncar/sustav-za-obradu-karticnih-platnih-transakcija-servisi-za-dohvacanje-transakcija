3857b558c5ffb36551b2547e65feee89cc117933
kmilos-foi <kmilos21@foi.hr>
2025-01-08 12:54:38 +0100

    MI5-88 Dodana metoda za dohvat merchanta po oib-u

--- src/main/java/foi/air/szokpt/transfetch/repositories/MerchantRepository.java
+++ src/main/java/foi/air/szokpt/transfetch/repositories/MerchantRepository.java
@@ -4,6 +4,9 @@ import foi.air.szokpt.transfetch.entities.Merchant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MerchantRepository extends JpaRepository<Merchant, Integer> {
+    Optional<Merchant> findByOib(String oib);
 }
