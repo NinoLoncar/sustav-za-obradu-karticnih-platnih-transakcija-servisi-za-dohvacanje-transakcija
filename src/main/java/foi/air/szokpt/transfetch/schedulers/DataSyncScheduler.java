487651ab0518f748c97b913e868a5be39f5ab358
kmilos-foi <kmilos21@foi.hr>
2025-01-08 16:26:41 +0100

    MI5-88 Ažuriran Scheduler

---
+++ src/main/java/foi/air/szokpt/transfetch/schedulers/DataSyncScheduler.java
@@ -0,0 +1,19 @@
+package foi.air.szokpt.transfetch.schedulers;
+
+import foi.air.szokpt.transfetch.services.DataFetchService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataSyncScheduler {
+    private final DataFetchService dataFetchService;
+
+    public DataSyncScheduler(DataFetchService fetchService) {
+        this.dataFetchService = fetchService;
+    }
+
+    @Scheduled(cron = "0 0 0,12 * * ?", zone = "Europe/Zagreb")
+    public void periodicDataFetch() {
+        dataFetchService.synchronizeData();
+    }
+}
