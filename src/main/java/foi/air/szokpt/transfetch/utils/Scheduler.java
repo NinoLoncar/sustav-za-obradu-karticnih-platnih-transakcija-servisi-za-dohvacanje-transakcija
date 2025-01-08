ae16e6b33ddeb4930a8ba13912a7350fd76156bd
kmilos-foi <kmilos21@foi.hr>
2025-01-08 12:50:23 +0100

    MI5-88 Kreiran Scheduler
    Schedulerova metoda periodicDataFetch periodično dohvaća podatke iz vanjskog sustava.

---
+++ src/main/java/foi/air/szokpt/transfetch/utils/Scheduler.java
@@ -0,0 +1,20 @@
+package foi.air.szokpt.transfetch.utils;
+
+import foi.air.szokpt.transfetch.services.DataFetchService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+    private final DataFetchService dataFetchService;
+
+    public Scheduler(DataFetchService fetchService) {
+        this.dataFetchService = fetchService;
+    }
+
+    //@Scheduled(cron = "0 0 1,13 * * ?", zone = "Europe/Zagreb")
+    @Scheduled(fixedRate = 30000)
+    public void periodicDataFetch() {
+        dataFetchService.fetchData();
+    }
+}
