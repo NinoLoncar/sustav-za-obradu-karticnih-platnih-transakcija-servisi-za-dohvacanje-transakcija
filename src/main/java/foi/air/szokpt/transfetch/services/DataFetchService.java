445d20850093aab2f74e72dc45fb0d30436fa35b
kmilos-foi <kmilos21@foi.hr>
2025-01-08 13:07:29 +0100

    MI5-88 Dodan API base URL u properties
    Base URL za API kojega pruža 7Pay dodan je u application.properties. Sada više nije potrebno pisati cijeli url u servisu, već se može koristiti varijabla za base url.

--- src/main/java/foi/air/szokpt/transfetch/services/DataFetchService.java
+++ src/main/java/foi/air/szokpt/transfetch/services/DataFetchService.java
@@ -1,6 +1,5 @@
 package foi.air.szokpt.transfetch.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import foi.air.szokpt.transfetch.dtos.responses.MerchantResponse;
 import foi.air.szokpt.transfetch.dtos.responses.MidResponse;
 import foi.air.szokpt.transfetch.dtos.responses.TidsResponse;
@@ -11,6 +10,7 @@ import foi.air.szokpt.transfetch.repositories.MerchantRepository;
 import foi.air.szokpt.transfetch.repositories.MidRepository;
 import foi.air.szokpt.transfetch.repositories.TidRepository;
 import foi.air.szokpt.transfetch.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -21,59 +21,56 @@ import java.util.List;
 @Service
 public class DataFetchService {
     private final RestTemplate restTemplate;
-    private final ObjectMapper objectMapper;
 
     private final TidRepository tidRepository;
     private final MidRepository midRepository;
     private final MerchantRepository merchantRepository;
     private final TransactionRepository transactionRepository;
 
+    @Value("${7pay.api.base.url}")
+    private String baseUrl;
+
     public DataFetchService(RestTemplate restTemplate,
                             TidRepository tidRepository,
                             MidRepository midRepository,
                             MerchantRepository merchantRepository,
-                            TransactionRepository transactionRepository,
-                            ObjectMapper objectMapper) {
+                            TransactionRepository transactionRepository) {
         this.restTemplate = restTemplate;
         this.tidRepository = tidRepository;
         this.midRepository = midRepository;
         this.merchantRepository = merchantRepository;
         this.transactionRepository = transactionRepository;
-        this.objectMapper = objectMapper;
     }
 
     private List<Tid> fetchTids() {
-        String url = "https://api-uat.7pay.hr/tss/v1/tids";
+        String url = baseUrl + "/tids";
         ResponseEntity<TidsResponse> response = restTemplate.getForEntity(url, TidsResponse.class);
 
         if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
             return response.getBody().getTids();
         } else {
-            System.err.println("Failed to fetch TIDs");
             return new ArrayList<>();
         }
     }
 
     private Mid fetchMidByTid(String tid) {
-        String url = "https://api-uat.7pay.hr/tss/v1/merchants/midBTid?tid=" + tid;
+        String url = baseUrl + "/merchants/midBTid?tid=" + tid;
         ResponseEntity<MidResponse> response = restTemplate.getForEntity(url, MidResponse.class);
 
         if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
             return response.getBody().getMid();
         } else {
-            System.err.println("Failed to fetch MID for TID: " + tid);
             return null;
         }
     }
 
     private Merchant fetchMerchantByMid(String mid) {
-        String url = "https://api-uat.7pay.hr/tss/v1/merchants/merchantByMid?mid=" + mid;
+        String url = baseUrl + "/merchants/merchantByMid?mid=" + mid;
         ResponseEntity<MerchantResponse> response = restTemplate.getForEntity(url, MerchantResponse.class);
 
         if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
             return response.getBody().getMerchant();
         } else {
-            System.err.println("Failed to fetch MID for TID: " + mid);
             return null;
         }
     }
@@ -81,7 +78,6 @@ public class DataFetchService {
     public void fetchData() {
         List<Tid> tids = fetchTids();
         for (Tid tid : tids) {
-            System.out.println("Processing TID: " + tid.getPosTid());
             Mid mid = fetchMidByTid(tid.getPosTid());
             if (mid != null) {
                 Merchant merchant = fetchMerchantByMid(mid.getPosMid());
