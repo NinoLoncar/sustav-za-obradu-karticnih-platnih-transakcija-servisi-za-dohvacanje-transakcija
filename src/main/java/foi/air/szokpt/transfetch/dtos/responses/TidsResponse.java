d42b759b2819a89753cd4292324ee59c8e292ed8
kmilos-foi <kmilos21@foi.hr>
2025-01-08 15:07:37 +0100

    MI5-88 Ažuriran TidsResponse

--- src/main/java/foi/air/szokpt/transfetch/dtos/responses/TidsResponse.java
+++ src/main/java/foi/air/szokpt/transfetch/dtos/responses/TidsResponse.java
@@ -1,10 +1,12 @@
 package foi.air.szokpt.transfetch.dtos.responses;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import foi.air.szokpt.transfetch.entities.Tid;
 
 import java.util.List;
 
 public class TidsResponse {
+    @JsonProperty("tids")
     private List<Tid> tids;
 
     public List<Tid> getTids() {
