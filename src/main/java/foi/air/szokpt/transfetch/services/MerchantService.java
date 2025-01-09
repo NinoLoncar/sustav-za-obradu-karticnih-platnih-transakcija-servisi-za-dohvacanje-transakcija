ff652e035ac3d1acf1820328606de688e9ee318c
kmilos-foi <kmilos21@foi.hr>
2025-01-09 19:59:50 +0100

    MI5-89 Kreirana servisna metoda za dohvat merchana
    Dohvaćanjem svih merchanata, dohvatiti će se i svi ostali podaci jer merchant sadrži više mid-ova, jedan mid više tid-ova, a tid više transakcija.

--- src/main/java/foi/air/szokpt/transfetch/services/MerchantService.java
+++ src/main/java/foi/air/szokpt/transfetch/services/MerchantService.java
@@ -4,6 +4,8 @@ import foi.air.szokpt.transfetch.entities.Merchant;
 import foi.air.szokpt.transfetch.repositories.MerchantRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MerchantService {
 
@@ -11,6 +13,10 @@ public class MerchantService {
 
     public MerchantService(MerchantRepository merchantRepository) {
         this.merchantRepository = merchantRepository;
+    }
+
+    public List<Merchant> getMerchants() {
+        return merchantRepository.findAll();
     }
 
     public Merchant saveMerchant(Merchant merchant) {
