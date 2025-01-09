053f98b612a7b7d95009e87267dc3aa51518fd3d
kmilos-foi <kmilos21@foi.hr>
2025-01-09 19:58:50 +0100

    MI5-89 Kreirana metoda za dohvat transakcija
    Metoda dohvaća transakcije prema tid-u i sve one transakcije koje su napravljene tijekom jučerašnjeg dana.

--- src/main/java/foi/air/szokpt/transfetch/repositories/TransactionRepository.java
+++ src/main/java/foi/air/szokpt/transfetch/repositories/TransactionRepository.java
@@ -1,11 +1,21 @@
 package foi.air.szokpt.transfetch.repositories;
 
+import foi.air.szokpt.transfetch.entities.Tid;
 import foi.air.szokpt.transfetch.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    @Query("SELECT t FROM Transaction t WHERE t.tid = :tid AND t.transactionTimestamp BETWEEN :startOfDay AND :endOfDay")
+    List<Transaction> findByTidAndTransactionTimestamp(
+            @Param("tid") Tid tid,
+            @Param("startOfDay") LocalDateTime startOfDay,
+            @Param("endOfDay") LocalDateTime endOfDay);
 }
